35de64982eb38d83cd850d564fdde306a5e98836
YavinOwens <yavinowens87@googlemail.com>
2024-12-19 20:24:10 +0000

    Remove BONUS directory from git tracking

--- BONUS/components/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-Components package for the Data Quality Dashboard.
-Contains reusable UI components used across different pages.
-""" 
--- BONUS/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-Utilities package for the Data Quality Dashboard.
-Contains helper functions and classes used throughout the application.
-""" 
